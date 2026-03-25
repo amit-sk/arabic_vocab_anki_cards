6c1caebdbfa8a6910d9a662d9fb4a5ff0fa2ec8f
Amit Shalvi Kali <a.shalvikali@gmail.com>
2026-03-25 19:37:56 +0200

    working on audio

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 DECK_NAME = 'arabic vocab'
@@ -33,6 +34,24 @@ def add_flashcard(front, back):
     return invoke("addNote", note=note)
 
 
+def add_audio_flashcard(audio_url, back, front=None):
+    note = {
+        "deckName": DECK_NAME,
+        "modelName": "Basic",
+        "fields": {
+            "Front": front if front is not None else "audio",
+            "Back": back
+        },
+        "tags": ["api", "audio"],
+        "audio": {
+            "url": audio_url,
+            "filename": audio_url.split('/')[-1],
+            "fields": ["Front"]
+        }
+    }
+    return invoke("addNote", note=note)
+
+
 def words_intro():
     with open('words_intro.txt', 'r', encoding="utf-8") as f:
         words = f.read().splitlines()
@@ -41,10 +60,26 @@ def words_intro():
             back = word[word.find('(') + 1 : word.find(')')]
             add_flashcard(front=front, back=back)
 
+def words_lesson_1():
+    with open('words_lesson1.txt', 'r', encoding="utf-8") as f:
+        words = f.read().splitlines()
+        for word in words:
+            front = word[:word.find('|')]
+            back = word[word.find('|') + 1:]
+            add_flashcard(front=front, back=back)
+
+def audio():
+    with open('audio/audio.json', 'r', encoding="utf-8") as f:
+        words = json.load(f)
+        for front, back in words.items():
+            add_audio_flashcard(audio_url=front, back=back)
 
 def main():
     # test connection
     print("AnkiConnect version:", invoke("version"))
+    # invoke("createDeck", deck=DECK_NAME)
+    # words_intro()
+    # words_lesson_1()
 
 
 if __name__ == "__main__":
